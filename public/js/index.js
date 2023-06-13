1e83c78f2b39184300fd3921dd04aa9e3c3832e5
Jordan <jlprogrammer_98@hotmail.com>
2023-06-13 19:37:11 +0100

    apply colour themes based on section (initial)

--- public/js/index.js
+++ public/js/index.js
@@ -1,4 +1,22 @@
-window.addEventListener("scroll", function() {
-    let header = document.querySelector(".main-header");
-    header.classList.toggle("sticky", window.scrollY > 0)
-})
+window.addEventListener("scroll", function () {
+  let header = document.querySelector(".main-header");
+  header.classList.toggle("sticky", window.scrollY > 0);
+});
+
+window.addEventListener("scroll", function () {
+  const parallaxSection = document.querySelector(".parallax-section");
+  const nav = document.querySelectorAll(".main-header.sticky .navigation a");
+  const logo = document.querySelector(".logo a");
+
+  if (parallaxSection.getBoundingClientRect().top < 155) {
+    nav.forEach((item) => {
+      item.classList.add("purple-neon");
+    });
+    logo.classList.add("purple-neon");
+  } else {
+    nav.forEach((item) => {
+      item.classList.remove("purple-neon");
+    });
+    logo.classList.remove("purple-neon");
+  }
+});
