b89cd3b6d06593e2e7667fcd1cabc7193fff5773
Jordan <jlprogrammer_98@hotmail.com>
2023-06-16 12:17:42 +0100

    refactored indexjs file

--- public/js/index.js
+++ public/js/index.js
@@ -1,22 +1,25 @@
-window.addEventListener("scroll", function () {
+const stickyNavBar = function () {
   let header = document.querySelector(".main-header");
   header.classList.toggle("sticky", window.scrollY > 0);
-});
+};
 
-window.addEventListener("scroll", function () {
+const passionSectionColourChange = function () {
   const parallaxSection = document.querySelector(".parallax-section");
   const nav = document.querySelectorAll(".main-header.sticky .navigation a");
   const logo = document.querySelector(".logo a");
 
   if (parallaxSection.getBoundingClientRect().top < 155) {
     nav.forEach((item) => {
-      item.classList.add("purple-neon");
+      item.classList.add("pink-neon");
     });
-    logo.classList.add("purple-neon");
+    logo.classList.add("pink-neon");
   } else {
     nav.forEach((item) => {
-      item.classList.remove("purple-neon");
+      item.classList.remove("pink-neon");
     });
-    logo.classList.remove("purple-neon");
+    logo.classList.remove("pink-neon");
   }
-});
+};
+
+window.addEventListener("scroll", stickyNavBar);
+window.addEventListener("scroll", passionSectionColourChange);
