0a186c66f7b935a49168dda202c93bb46a6e463f
Jordan <jlprogrammer_98@hotmail.com>
2023-06-21 11:19:43 +0100

    mobile hamburger click event setup

--- public/js/index.js
+++ public/js/index.js
@@ -3,6 +3,11 @@ const stickyNavBar = function () {
   header.classList.toggle("sticky", window.scrollY > 0);
 };
 
+const hamburgerClick = function () {
+  const hamburgerIcon = document.querySelector("#hamburger-icon");
+  hamburgerIcon.classList.toggle("open");
+};
+
 const passionSectionColourChange = function () {
   const parallaxSection = document.querySelector(".parallax-section");
   const nav = document.querySelectorAll(".main-header.sticky .navigation a");
@@ -22,7 +27,9 @@ const passionSectionColourChange = function () {
 };
 
 const experienceSectionColourChange = function () {
-  const parallaxSection = document.querySelector(".parallax-section~.parallax-section");
+  const parallaxSection = document.querySelector(
+    ".parallax-section~.parallax-section"
+  );
   const nav = document.querySelectorAll(".main-header.sticky .navigation a");
   const logo = document.querySelector(".logo a");
 
@@ -33,7 +40,6 @@ const experienceSectionColourChange = function () {
     });
     logo.classList.remove("pink-neon");
     logo.classList.add("golden-orange");
-
   } else {
     nav.forEach((item) => {
       item.classList.remove("golden-orange");
@@ -43,6 +49,6 @@ const experienceSectionColourChange = function () {
 };
 
 window.addEventListener("scroll", stickyNavBar);
+window.addEventListener("click", hamburgerClick);
 window.addEventListener("scroll", passionSectionColourChange);
 window.addEventListener("scroll", experienceSectionColourChange);
-
